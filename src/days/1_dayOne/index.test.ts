e7ed622e26beb557688dd040ab5396880fde3980
Lewis Nelson <lewis@lewnelson.com>
2023-12-01 09:54:14 +0000

    day 1 complete

--- src/days/1_dayOne/index.test.ts
+++ src/days/1_dayOne/index.test.ts
@@ -3,12 +3,31 @@ import { TestCase } from '~/types';
 
 describe('dayOne', () => {
   const testCases: TestCase[] = [
-    { description: 'not implemented', input: '', expectedOutput: 'not implemented' },
+    { description: 'given the sample input the expected output is 142', input: `1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet`, expectedOutput: 142 },
   ];
 
   describe('partOne', () => {
     test.each(testCases)('$description', ({ input, expectedOutput }) => {
       expect(dayOne.partOne(input)).toBe(expectedOutput);
+    });
+  });
+
+  describe('partTwo', () => {
+    const testCases: TestCase[] = [
+      { description: 'given the sample input the expected output is 281', input: `two1nine
+      eightwothree
+      abcone2threexyz
+      xtwone3four
+      4nineeightseven2
+      zoneight234
+      7pqrstsixteen`, expectedOutput: 281 },
+    ];
+
+    test.each(testCases)('$description', ({ input, expectedOutput }) => {
+      expect(dayOne.partTwo(input)).toBe(expectedOutput);
     });
   });
 });
--- src/days/1_dayOne/index.ts
+++ src/days/1_dayOne/index.ts
@@ -1,6 +1,51 @@
 // https://adventofcode.com/2023/day/1
 import { Main } from '~/types';
 
+const getCalibrationValue = (line: string): number | undefined => {
+  const numbers = line.match(/\d{1}/g) ?? [];
+  if (numbers.length === 0) return undefined;
+  let first: string;
+  let last: string;
+  if (numbers.length === 1) {
+    first = numbers[0];
+    last = numbers[0];
+  } else {
+    first = numbers[0]!;
+    last = numbers[numbers.length - 1];
+  }
+
+  return parseInt(`${first}${last}`, 10);
+};
+
 export const partOne: Main = input => {
-  return 'not implemented';
+  return input.split('\n').reduce((sum, line) => {
+    const calibrationValue = getCalibrationValue(line);
+    if (calibrationValue === undefined) return sum;
+    return sum + calibrationValue;
+  }, 0);
+};
+
+const numbersAsText = {
+  zero: '0',
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+} as const;
+
+export const partTwo: Main = input => {
+  return input.split('\n').reduce((sum, line) => {
+    const numericalLine = line.replace(new RegExp(`(${Object.keys(numbersAsText).join('|')})`, 'g'), match => {
+      return numbersAsText[match as keyof typeof numbersAsText] ?? '';
+    });
+
+    const calibrationValue = getCalibrationValue(numericalLine);
+    if (calibrationValue === undefined) return sum;
+    return sum + calibrationValue;
+  }, 0);
 };
